0355a92c91895db798758128552d4a50f1991039
giannisil <iliadis_giannis@hotmail.com>
2016-06-09 00:25:37 +0300

    Implements validation of login credentials from form with DB

--- usersList.php
+++ usersList.php
@@ -1,3 +1,46 @@
+<?php
+	// If we came to the page after a session login
+	if ( (!empty($_POST)) && isset($_POST['username']) && isset($_POST['password']) ) {
+		// Check in Database if login credentials are valid
+		
+		// Credentials for Database connection
+		$servername = "localhost";
+		$username = "3519";
+		$password = "3519-Ix";
+		$DBName = "db3519";
+		$tableName = "data3";
+		// Connecto to DB
+		$conn = new mysqli($servername, $username, $password, $DBName);
+		// Construct SQL query
+		$sqlCheckCredentials = "SELECT password FROM ". $tableName ." WHERE username='". $_POST['username'] ."';";
+		// Send Query
+		$result = $conn->query($sqlCheckCredentials);
+		if (!$result === TRUE) {
+			echo "Error: " . $DBName . ". " . $conn->error . "<br/> SQL command: " . $sqlCheckCredentials;
+		}
+		echo "result->num_rows = ".$result->num_rows; //TODO DELETE DEBUG
+		// Check if any matching passwords were found in DB
+		if ($result->num_rows > 0) {
+			$passwordMatched = false;
+			while($row = $result->fetch_assoc()) {
+				echo "row['password'] = ".$row["password"]; //TODO DELETE DEBUG
+				echo "post['password'] = ".$_POST["password"]; //TODO DELETE DEBUG
+				if ($row["password"] == $_POST['password']) {
+						$passwordMatched = true;
+					break;
+				}
+			}
+			if ($passwordMatched) {
+				// If matching password for user is found in DB then redirect to userList without POST requests
+				header('Location: usersList.php');
+			} else { 
+				// Else, if no matching password is found for user, then redirect back to login page
+				header('Location: class.html');
+			}
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <meta charset="UTF-8" />
 <html>
@@ -32,7 +75,7 @@
 			// output data of each row
 			while($row = $result->fetch_assoc()) {
 				echo '<div id="'. $row["id"] .'"><a href="userDetails.php?id='. $row["id"] .'">AM</a>: ' 
-						. $row["id"]. " - Onoma: " . $row["name"]. " " . $row["surname"]. "</div><br>"; 
+						. $row["id"]. " | Onoma: " . $row["name"]. " " . $row["surname"]. "</div><br>"; 
 			}
 		} else {
 			echo "0 results";
